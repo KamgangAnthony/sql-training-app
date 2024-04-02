d5d2d15e99ec80e2c603a69efc14c8db4a896969
kamgang <kamgang@DESKTOP-LIG1C10>
2024-04-02 19:06:45 +0200

    modified files to pass quality tests

--- init_db.py
+++ init_db.py
@@ -6,13 +6,13 @@ con = duckdb.connect("data/exercises_sql_tables.duckdb", read_only=False)
 
 # --------------------------------------------------
 # EXERCISES LIST
-#---------------------------------------------------
+# ---------------------------------------------------
 
 data = {
     "theme": ["cross_joins", "window_functions"],
     "exercise_name": ["beverages_and_food", "simple_window"],
     "tables": [["beverages", "food_items"], "simple_window"],
-    "last_reviewed": ["1970-01-01", "1970-01-01"]
+    "last_reviewed": ["1970-01-01", "1970-01-01"],
 }
 memory_state_df = pd.DataFrame(data)
 con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
@@ -20,7 +20,7 @@ con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_sta
 
 # --------------------------------------------------
 # CROSS JOIN EXERCISES
-#---------------------------------------------------
+# ---------------------------------------------------
 
 CSV = """
 beverage,price
